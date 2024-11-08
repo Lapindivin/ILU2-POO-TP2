e0ca15dac05a330847d14a8d7a22d38e47b05bad
Laîndivin <nsi2ceninileny@gmail.com>
2024-11-08 11:58:28 +0100

    TP3.2

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,22 +13,31 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
 		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.println("je suis desolé " + nomVendeur + " il faut etre du village .");
 			
 		}else {
+			System.out.println("je vais me renseigner si il reste un etal");
 			if (!controlPrendreEtal.resteEtals()) {
-				
+				System.out.println("desole tout est occuper");
 			} else {
-				if (numeroEtal!=-1) {
-					
-				}
-
+				installerVendeur(nomVendeur);
 			}
 		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("il reste un etal pour vous ");
+		String produit ;
+		produit = Clavier.entrerChaine("quel est votre produit ?");
+		int nbproduit = 0;
+		nbproduit = Clavier.entrerEntier("Quelle est le nb de produit ? ");
+		int numeroEtal=0;
+		numeroEtal=controlPrendreEtal.prendreEtal(nomVendeur, produit, nbproduit);
+		
+		
+		if (numeroEtal!=-1) {
+			System.out.println("le vendeur " + nomVendeur + " s'est installe a l'etal numero " + numeroEtal);
+		}
 	}
 }
